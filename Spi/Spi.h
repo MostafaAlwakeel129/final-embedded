e48126d817a14d02b514c9ed767c8d9b6c8f985f
MostafaAlwakeel129 <mostafa.ibrahim041@eng-st.cu.edu.eg>
2026-05-10 16:38:09 +0300

    spi done

--- Spi/Spi.h
+++ Spi/Spi.h
@@ -1,8 +1,5 @@
 /**
  * Spi.h
- *
- *  Created on: Wed May 24 2023
- *  Author    : Abdullah Darwish
  */
 
 #ifndef SPI_H
@@ -21,8 +18,7 @@ typedef struct
     uint32 TXCRCR;
     uint32 I2SCFGR;
     uint32 I2SPR;
-}SpiType;
-
+} SpiType;
 
 /* Master or Slave */
 #define SPI_SLAVE  0
@@ -33,16 +29,26 @@ typedef struct
 #define SPI_IDLE_HIGH 1
 
 /* Clock Phase */
-#define SPI_SAMPLE_FIRST_TRANSITION  0 // Read or Capture on first transition
+#define SPI_SAMPLE_FIRST_TRANSITION  0
 #define SPI_SAMPLE_SECOND_TRANSITION 1
 
 #define SPI_OK     0U
 #define SPI_NOK    1U
 
+#define SPI_PACKET_LEN 8U
 
+typedef void (*SpiCallback)(void);
 
+/* Your original Init */
 void Spi1_Init(uint8 MasterSlave, uint8 ClkPol, uint8 ClkPhase);
 
+/* Your original blocking function (good for debugging) */
 uint8 Spi1_TransmitReceiveByte(uint8 TxData, uint8* RxData);
+
+/* --- NEW NON-BLOCKING FUNCTIONS --- */
+uint8 Spi1_MasterTransferAsync(uint8 *txBuf, uint8 *rxBuf, uint8 len, SpiCallback Callback);
+void Spi1_SlavePreload(uint8 *txBuf, uint8 len);
+void Spi1_SlaveEnableRx(SpiCallback Callback);
+void Spi1_SlaveGetRxBuffer(uint8 *out);
 
 #endif /* SPI_H */
